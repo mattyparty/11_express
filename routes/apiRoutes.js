a250ea6fcf0d3e5663921e5756896dd80879a2a3
Matty Party <matthewpewewardy@gmail.com>
2020-11-25 16:09:27 -0600

    got the post api working and creating a new Javascript file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,50 @@
 const router = require("express").Router();
 const notesData = require("../db/db");
 const path = require("path");
+const fs = require("fs");
 
-router.get("/notes", function (req, res) {
-  res.json(notesData);
-});
+router
+  .get("/notes", function (req, res) {
+    res.json(notesData);
+  })
+  .post("/notes", (req, res) => {
+    fs.readFile("./db/db.json", (err, data) => {
+      if (err) throw err;
+      const dbFile = JSON.parse(data);
+      dbFile.push(req.body);
+      //console.log(dbFile);
+      //console.log(req.body);
+      ////this all prolly needs to be in here
+      fs.writeFileSync("./db/db.json", JSON.stringify(dbFile), function (err) {
+        if (err) throw err;
+        //console.log("The file has been saved!");
+      });
+    });
 
-// router.post("/notes", function (req, res) {
-//   store
-//     .addNote(req.body)
-//     .then((notes) => res.json(notes))
-//     .catch((err) => res.status(500).json(err));
+    res.end();
+  });
+//code here
+//1. find out what data they are sending  us ()
+//console.log(req.body);
+//   fs.readFile("./db/db.json", function (err, data) {
+//    // var notesJson = JSON.parse(data);
+//     //notesJson.push(req.body);
+//     //console.log(notesJson);
+//     // fs.writeFile("./db/db.json", JSON.stringify(notesJson));
+//     //console.log(JSON.stringify(notesJson));
+
+//   //   fs.writeFileSync("./db/db.json", JSON.stringify(notesJson), (err) => {
+//   //     if (err) throw err;
+//   //     console.log("The file has been saved!");
+//   // });
+//   //res.end();
 // });
 
-// // router.delete("/notes/:title", function (req, res) {
-// //   store
-// //     .deleteNotes(req.params.title)
-// //     .then(() => res.json({ ok: true }))
-// //     .catch((err) => res.status(500).json(err));
-// // });
+// console.log(notesData);
+// console.log(req.body);
+//req.body and push to the static json file
+// read data, set up as an array, then push to an array
+
+//vvvstatus code
+
 module.exports = router;

b782d0e0648ea7df3b20ae8d169d829c5f8d814b
Matty Party <matthewpewewardy@gmail.com>
2020-11-25 22:30:41 -0600

    final tweaks

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,47 +1,16 @@
 const router = require("express").Router();
 const notesData = require("../db/db.json");
 const { v4: uuidv4 } = require("uuid");
-//const path = require("path");
 const fs = require("fs");
 
-/////////////////////////////////////////////////////////////
-// router.get("/api/notes", function (req, res) {
-//   res.json(notesData);
-// });
-
-// //post new notes works
-// router.post("/api/notes", (req, res) => {
-//   req.body.id = uuidv4();
-//   notesData.push(req.body);
-
-//   fs.writeFile("./db/db.json", JSON.stringify(notesData), (err) => {
-//     if (err) throw err;
-//   });
-
-//   res.json(notesData);
-
-// });
-
+////////////////////////////////////////////
+//API GET request called from index.js
 router
-  .delete("/api/notes/:id", (req, res) => {
-    const removeID = req.params.id;
-    for (let i = 0; i < notesData.length; i++) {
-      if (notesData[i].id === removeID) {
-        notesData.splice(i, 1);
-      }
-    }
-    fs.writeFile("./db/db.json", JSON.stringify(notesData), (err) => {
-      if (err) throw err;
-    });
-    //removeData = notesData;
-    res.json(notesData);
-  })
-  ////////////////////////////////////////////
   .get("/api/notes", function (req, res) {
     res.json(notesData);
   })
 
-  //post new notes works
+  //API POST request called from index.js
   .post("/api/notes", (req, res) => {
     req.body.id = uuidv4();
     notesData.push(req.body);
@@ -52,7 +21,8 @@ router
 
     res.json(notesData);
   })
-
+  //API DELETE request called from index.js takes the ID of the selected delte items and loops through
+  // notes data and delete/splice's it from the array
   .delete("/api/notes/:id", (req, res) => {
     const removeID = req.params.id;
     for (let i = 0; i < notesData.length; i++) {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,10 +7,10 @@ router.get("/notes", (_req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
-//might need this for heroku
-// router.get("/", function(req, res) {
-//   res.json(path.join(__dirname, "public/index.html"));
-//   });
+//Code need for heroku
+router.get("/", function (req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
 
 router.get("*", (_req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
